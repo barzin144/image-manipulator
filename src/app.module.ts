aa015de78894ffbac5e38038ca3ed5f67685a81a
Barzin <barzin.assa@elfo.com>
2022-04-08 19:09:20 +0800

    create image with dices fixed some issues in pixelate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { Module } from "@nestjs/common";
+import { DiceModule } from "./dice/dice.module";
+import { PixelateModule } from "./pixelate/pixelate.module";
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PixelateModule, DiceModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { json } from "body-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: "5mb" }));
   await app.listen(8080);
 }
 bootstrap();
