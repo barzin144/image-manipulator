91f62e3da775035c64a4f3b89d8a4a4a6772dbaf
Barzin <barzin.assa@elfo.com>
2022-04-15 12:41:33 +0800

    add webpack

--- Frontend/webpack.prod.js
+++ Frontend/webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
